3622bd99dbdfc1f5d246db776a8d6b13c21b9661
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-11-24 21:35:19 -0500

    Secured /chat route

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,8 +1,26 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
 import React from "react";
+
+import { useSession } from "next-auth/react"
 
 import styles from "../styles/chat.module.css";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p>Loading</p>
+  }
+
+  if (status === "unauthenticated") {
+    router.replace("/api/auth/signin");
+    return (<p>Access Denied</p>);
+  }
+
   return (
     <>
       <div className={styles.room}>
