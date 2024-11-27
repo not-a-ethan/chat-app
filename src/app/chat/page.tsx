d655867f1b2145f432c594b2b01908cf162f71bc
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-11-27 18:55:56 -0500

    UI of sending a message

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import Image from 'next/image';
 
 import React from "react";
 
 import { useSession } from "next-auth/react"
 
 import styles from "../styles/chat.module.css";
+
+import { send } from 'process';
 
 export default function Home() {
   const { data: session, status } = useSession()
@@ -36,16 +39,21 @@ export default function Home() {
       <div className={styles.room}>
         <div className={styles.messages}>
           <p>Messages</p>
+
+          <div className={`${styles.messageInput}`} >
+            <textarea className={`${styles.textArea}`}></textarea>
+            <img
+              src={`/arrow-right.svg`}
+              alt="Right arrow to send the message"
+              className={`${styles.sendButton}`}
+            />
+          </div>
         </div>
 
         <div className={styles.people}>
           <p>People in room</p>
         </div>
       </div>
-
-      <script>
-        fetch("http://localhost:3001/api/message/create")
-      </script>
     </>
   );
 }
