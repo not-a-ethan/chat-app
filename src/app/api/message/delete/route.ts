5608dd03505d7329367fe5056ae690ffc0ad20ca
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-11-27 16:17:09 -0500

    Delete message route

---
+++ src/app/api/message/delete/route.ts
@@ -0,0 +1,55 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getToken } from "next-auth/jwt"
+
+import { getAll } from "@/app/database/get"
+import { changeDB } from "@/app/database/change"
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    const token = await getToken({ req })
+
+    if (!token) {
+        return NextResponse.json(
+            JSON.stringify(
+                {"message": "Not authenticated"},
+            ),
+            {status: 403}
+        );
+    }
+
+    let externalID;
+    let id;
+
+    if (token.sub) {
+        externalID = token.sub;
+        id = await getAll(`SELECT * FROM users WHERE externalID=$id`, {"$id": externalID});
+        id = id[0].id;
+    } else {
+        externalID = NaN;
+        id = -1;
+    }
+    
+
+    const body = await req.json();
+    const messageID: Number = body["messageID"];
+
+    const messageQuery: String = "SELECT * FROM messages WHERE 'author'=$id AND 'id'=$messageid";
+    const message = await getAll(messageQuery, {"$id": id, "$messageID": messageID})
+
+    if (message.length === 0) {
+        return NextResponse.json(
+            JSON.stringify({
+                error: "You do not own that message"
+            }),
+            { status: 403 }
+        )
+    }
+
+    const query: String = "DELETE FROM messages WHERE 'messageID'=$messageID AND 'author'=$authorID"
+    const response = await changeDB(query, {$messageID: messageID, $authorID: id})
+
+    return NextResponse.json(
+        JSON.stringify(response),
+        {status: 200}
+    )
+}
