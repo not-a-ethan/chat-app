a1967587240129b197e38027c12c5e7d312f0010
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-11-29 11:21:10 -0500

    API route to get active users in a specific room

---
+++ src/app/api/rooms/activeMembers/route.ts
@@ -0,0 +1,78 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getToken } from "next-auth/jwt"
+
+import { getAll } from "@/app/database/get"
+
+import { updateActivity } from '@/lib/updateActivity'
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    const token = await getToken({ req })
+
+    if (!token) {
+        return NextResponse.json(
+            JSON.stringify(
+                {"message": "Not authenticated"},
+            ),
+            {status: 403}
+        );
+    }
+
+    let externalID;
+    let username;
+
+    if (token.sub) {
+        externalID = token.sub;
+        username = await getAll(`SELECT * FROM users WHERE externalID=${externalID}`);
+        username = username[0].username;
+    } else {
+        externalID = NaN;
+        username = "";
+    }
+
+    // Get room ID
+    const searchParams = req.nextUrl.searchParams;
+    const roomID: number = Number(searchParams?.get("roomID"));
+
+    // Validates room ID
+    if (roomID < 1) {
+        return NextResponse.json(
+            JSON.stringify({
+                "error": "Room ID needs to be more than 1"
+            }),
+            { status: 400 }
+        )
+    }
+
+    // Checks if user is in the room
+    let rooms = await getAll(`SELECT * FROM users WHERE username=$username`, {"$username": username});
+    rooms = rooms[0]["rooms"];
+    
+    if (!rooms.includes(roomID)) {
+        return NextResponse.json(
+            JSON.stringify({
+                "error": "Not in room"
+            }),
+            { status: 403 }
+        )
+    }
+
+    updateActivity(username);
+
+    // Users active in the past 5 min and in a specific room
+    const query = `SELECT * FROM users WHERE rooms LIKE CONCAT('%,', $room, ',%') AND recentlyActive > (strftime('%s', 'now') - 300000)`
+
+    let users = await getAll(query, {"$room": roomID});
+    const usernames = [];
+
+    for (let i = 0; i < users.length; i++) {
+        usernames.push(users[i].username);
+    }
+    
+    return NextResponse.json(
+        JSON.stringify({
+            "users": usernames
+        }),
+        {status: 200}
+    )
+}
