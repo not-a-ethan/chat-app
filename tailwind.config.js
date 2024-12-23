fb7195d66a64a4f8fdbb8512b94b3a9bcb69865d
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-12-23 15:46:22 -0500

    Finished light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,57 +8,10 @@ const config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    light: {
-      
-    },
-    dark: {
-      primary: {
-        background: "#1C2B4F",
-        foreground: "#78B6E2"
-      }
-    },
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui({
-    prefix: "nextui", // prefix for themes variables
-    addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
-    defaultTheme: "light", // default theme from the themes object
-    defaultExtendTheme: "light", // default theme to extend on custom themes
-    layout: {}, // common layout tokens (applied to all themes)
-    themes: {
-      /*
-      light: {
-        colors: {
-          background: "#338ef7", // or DEFAULT
-          foreground: "#27272a", // or 50 to 900 DEFAULT
-          primary: {
-            //... 50 to 900
-            foreground: "#338ef7",
-            DEFAULT: "#006FEE",
-          },
-          secondary: {
-            colors: {
-              foreground: "#005BC4"
-            }
-          }
-          // ... rest of the colors
-        },
-      },
-      dark: {
-        colors: {
-          background: "#002e62", // or DEFAULT
-          foreground: "#ECEDEE", // or 50 to 900 DEFAULT
-          primary: {
-            //... 50 to 900
-            foreground: "#FFFFFF",
-            DEFAULT: "#002e62",
-          },
-        },
-      }
-      */
-    }
-  })]
+  plugins: [nextui()]
 }
 
 export default config;
